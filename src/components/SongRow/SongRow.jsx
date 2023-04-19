d6f94c36027a25f6f2f339faa470d6198ee61ae6
David Smith <dnsmith124@gmail.com>
2023-04-18 20:03:35 -0500

    adjust styling to more closely resemble Spotify's web UI

--- src/components/SongRow/SongRow.jsx
+++ src/components/SongRow/SongRow.jsx
@@ -1,19 +1,30 @@
 
-const SongRow = ({ track }) => {
+const SongRow = ({ track, index }) => {
 
   return (
-    <a href={track?.external_urls["spotify"]} className="song-row" target="_blank" rel="noreferrer">
+    <a 
+      href={track?.external_urls["spotify"]} 
+      className="grid p-[5px] gap-y-[20px] gap-x-[15px] transition-colors 
+        grid-cols-[40px_40px_1fr_.5fr] my-5px hover:bg-spotify-hover-gray
+        items-center" 
+      target="_blank" 
+      rel="noreferrer"
+      >
+      <p className="whitespace-nowrap text-center">{index + 1}.</p>
       {
         track.album.images[0] !== undefined &&
-        <img src={track.album.images[0].url} alt="" className="song-row__album" />
+        <img src={track.album.images[0].url} alt="" className="w-[40px]" />
       }
-      <div className="song-row__info">
-        <h1>{track.name}</h1>
+      <div>
+        <p>{track.name}</p>
         <p>
-          {track.artists.map((artist, i) => artist.name).join(", ")} -{" "}
-          {track.album.name}
+          {track.artists.map((artist, i) => artist.name).join(", ")}
         </p>
       </div>
+      <div>
+        {track.album.name}
+      </div>
+
     </a>
   );
 }
--- src/components/SpotifyBody/SpotifyBody.jsx
+++ src/components/SpotifyBody/SpotifyBody.jsx
@@ -18,24 +18,25 @@ const SpotifyBody = ({ spotify }) => {
   }, [currentPlaylistID, dispatch, spotify, token]);
 
   return (
-    <div className="dark:bg-spotify-dark-gray py-[20px] bg-gradient-to-b from-spotify-dark-red to-spotify-dark-gray">
+    <div className="dark:bg-spotify-dark-gray py-[20px] dark:bg-gradient-to-b dark:from-spotify-dark-red dark:to-spotify-dark-gray">
       <SpotifyHeader />
-      {currentPlaylist != null
-        ? <div>
-            <div className="">
-              <img src={currentPlaylist?.images[0]?.url} alt=""  className=""/>
-              <div className="">
-                <h2><a href={currentPlaylist?.external_urls["spotify"]} target="_blank" rel="noreferrer">{currentPlaylist?.name}</a></h2>
+      {currentPlaylist !== null
+        ? <div className="px-[30px]">
+            <div className="flex">
+              <img src={currentPlaylist?.images[0]?.url} alt=""  className="w-[250px] mr-[25px]"/>
+              <div className="grid content-end">
+                <p className="mb-[25px]">Playlist</p>
+                <h2 className="text-[60px]"><a href={currentPlaylist?.external_urls["spotify"]} target="_blank" rel="noreferrer">{currentPlaylist?.name}</a></h2>
                 <p>{currentPlaylist?.description}</p>
               </div>
             </div>
-            <div className="">
+            <div className=" pt-[30px]">
               {currentPlaylist?.tracks.items.map((item, i) => (
-                <SongRow track={item.track} key={i} />
+                <SongRow track={item.track} index={i} key={i} />
               ))}
             </div>
           </div>
-        : <h2>No Playlist Selected</h2>  }
+        : <h2 className="text-center text-[40px] pt-[50px]">No Playlist Selected</h2>  }
     </div>
   )
 }
