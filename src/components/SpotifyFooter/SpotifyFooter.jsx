52ee4f11069bfc94126cd08c520f4d4b9d0562a9
David Smith <dnsmith124@gmail.com>
2023-04-27 17:35:28 -0500

    dark mode styling update

--- src/components/SpotifyFooter/SpotifyFooter.jsx
+++ src/components/SpotifyFooter/SpotifyFooter.jsx
@@ -3,7 +3,7 @@ import SpotifyPlayer from "../SpotifyPlayer/SpotifyPlayer";
 const SpotifyFooter = () => {
   
   return (
-    <footer className=" col-span-2 sticky bottom-0 bg-[#181818]">
+    <footer className=" col-span-2 sticky bottom-0 dark:bg-[#181818] bg-white border-t-[1px] dark:border-t-[0]">
       <hr className="h-px border-0 my-0" />
       <SpotifyPlayer/>
     </footer>
